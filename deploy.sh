4167a7668d3aaa4f27930e1fe2e50c7232828dcd
Godson <eaziguy13@gmail.com>
2026-04-15 22:08:35 +0000

    fixing permissions

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,9 @@ sudo rm -f /var/www/html/index.html /var/www/html/index.nginx-debian.html <<EOF
 <h1> CI/CD WORKING </h1>
 <p> This updated automatically via CI/CD!</p>
 EOF'
+
+# Give the web server permission to read the folder
+sudo chown -R www-data:www-data /var/www/html
+sudo chmod -R 755 /var/www/html
+
+echo "Done!"
