c53e74ed1d98b840dc84d69caa5da08a5ba54ae9
Godson <eaziguy13@gmail.com>
2026-04-15 16:42:32 +0000

    test elastic ip

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ sudo rm -f /var/www/html/index.html /var/www/html/index.nginx-debian.html
 # This creates the CORRECT file name
 sudo bash -c 'cat <<EOF > /var/www/html/index.html
 <h1> CI/CD WORKING </h1>
-<p> This updated automatically via GitHub Actions! </p>
+<p> This updated automatically via GitHub Actions Actively working baby! </p>
 EOF'
 
 echo "Done!"
