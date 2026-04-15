ce7c0f569ad8b658b4393f94851a9e2c2e4fb5d8
Godson <eaziguy13@gmail.com>
2026-04-15 15:45:15 +0000

    test cicd

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,8 @@ sudo systemctl start nginx
 
 echo "Deploying website....."
 sudo bash -c 'cat > /var/www/html/indexc.nginx-debian.html <<EOF
-<h1> Automated Devops Site </h1>
-<p1>This was deployed using aa bash script </p>
+<h1> CI/CD WORkING </h1>
+<p1>This updated automatically </p>
 EOF'
 
 echo "Done!"
