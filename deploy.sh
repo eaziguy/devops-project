18e992a541a6cf78be4abd74c57b46a23fa78026
Godson <eaziguy13@gmail.com>
2026-04-15 21:48:27 +0000

    improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-echo "Updating server...."
-sudo apt update -y
+echo "Deploying website..."
 
-echo "Installing nginx..."
-sudo apt install nginx -y
-
-echo "Starting nginx...."
-sudo systemctl start nginx
-sudo systemctl enable nginx
-
-echo "Deploying website...."
-# This line deletes the old file so the new one can take over
-sudo rm -f /var/www/html/index.html /var/www/html/index.nginx-debian.html
-
-# This creates the CORRECT file name
-sudo bash -c 'cat <<EOF > /var/www/html/index.html
+sudo bash -c 'cat > /var/www/html/index.html/index.nginx-debian.html <<EOF
 <h1> CI/CD WORKING </h1>
-<p> This updated automatically via GitHub Actions Actively working baby! </p>
+<p> This updated automatically via CI/CD!</p>
 EOF'
-
-echo "Done!"
