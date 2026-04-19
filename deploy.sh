17cfb20fae130de23f1ac8ea6ad1e2090563dd95
Godson <eaziguy13@gmail.com>
2026-04-19 18:52:36 +0000

    switch to docker deployment

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# Force the standard path
+# Force the path so GitHub can see it
 export PATH=$PATH:/usr/bin
 
 echo "Pulling latest code..."
 git pull origin main
 
-echo "Cleaning up old container..."
+echo "Stopping old container..."
 docker stop my-app || true
 docker rm my-app || true
 
-echo "Building and Running..."
+echo "Building new image..."
 docker build -t my-devops-app .
+
+echo "Running new container..."
 docker run -d -p 80:80 --name my-app my-devops-app
