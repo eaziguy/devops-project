8b2619280b1994b66ed0382a015c86f7f6f7aad8
Godson <eaziguy13@gmail.com>
2026-04-19 18:19:23 +0000

    Switch to Docker DEployment

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
+# Force the script to look in every possible folder for Docker
+export PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/snap/bin
 
 echo "Pulling latest code..."
 git pull origin main
 
-echo "Stopping old container..."
-# We use 'docker' without 'sudo' now
+echo "Cleaning up..."
 docker stop my-app || true
 docker rm my-app || true
 
-echo "Building new image..."
+echo "Building and Running..."
 docker build -t my-devops-app .
-
-echo "Running new container..."
 docker run -d -p 80:80 --name my-app my-devops-app
