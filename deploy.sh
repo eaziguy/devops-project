d5ba74001059e6693ce12b156dcd8a099e83eea7
Godson <eaziguy13@gmail.com>
2026-04-20 18:53:00 +0000

    Switch to Docker Compose

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-# Force the tool map
-export PATH=$PATH:/usr/bin:/usr/local/bin
 
 echo "Pulling latest code..."
-git pull origin main
+git pull 
 
 echo "Stopping old container..."
-# Using sudo here ensures the daemon connection never fails
-sudo docker stop my-app || true
-sudo docker rm my-app || true
+sudo docker compose down
 
-echo "Building and Running..."
-sudo docker build -t my-devops-app .
-sudo docker run -d -p 80:80 --name my-app my-devops-app
+echo "Starting with Docker Compose"
+sudo docker compose up -d --build
