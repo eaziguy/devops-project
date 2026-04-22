0e24993379cda0350072da08646478d4f25c9661
Godson <eaziguy13@gmail.com>
2026-04-22 12:09:07 +0000

    deploy app

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
+const fs = require('fs')
+
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('🚀 Hello from your Node.js app with CI/CD!');
+
+  if (req.url === "/") {
+  const html = fs.readFileSync("index.html");
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(html);
+  }
+
+  else if (req.url === "/api") {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+      message: "This is your API 🚀",
+      status: "success"
+    }));
+  }
+
+  else {
+    res.writeHead(404);
+    res.end('Not Found');
+  }
+
 });
 
 server.listen(3000, () => {
   console.log('Server running on port 3000');
 });
+
