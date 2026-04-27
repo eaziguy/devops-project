43393f2b878437041ba0761ad185822fee2eb998
Godson <eaziguy13@gmail.com>
2026-04-27 09:50:35 +0000

    test real deployment

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const server = http.createServer((req, res) => {
   else if (req.url === "/api") {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({
-      message: "CI/CD API working perfectly 🚀",
+      message: "CI/CD is  working perfectly 🚀",
       status: "success"
     }));
   }
